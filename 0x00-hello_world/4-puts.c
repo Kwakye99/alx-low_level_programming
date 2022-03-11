69a394e50b0d6d06912eb39faadf83d72c23456c
Kwakye99 <kwakyedarko@gmail.com>
2022-03-11 10:43:25 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/* This C programme prints exactly 
-* "rogramming is like building a multilingual puzzle
+/** This C programme prints exactly 
+* "programming is like building a multilingual puzzle
 * it uses the puts command and not the printf
 * returns o
 */
 
-int main(void){
-puts ("\"Programming is like building a multilingual puzzle");
-return 0;
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 
 }
