ab9276d9ac112d7cc4db6f40fdd1e60d1ac46d10
Kwakye99 <kwakyedarko@gmail.com>
2022-03-12 14:21:22 -0800

    Numbeerz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int p
+for (p = '0'; p <= 9; p++)
+{
+putchar(c);
+}
+print('\n');
+}
