d25e96b98a95de3713f0dc5b466884cafcec0098
Kwakye99 <kwakyedarko@gmail.com>
2022-03-12 14:52:12 -0800

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main = Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char j;
+for(j = 'z'; j >= 'a' j--)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
