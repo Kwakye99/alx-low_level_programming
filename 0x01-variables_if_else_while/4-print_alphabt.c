4dc150309c72a81dff1e3def46cc7c724fe05558
Kwakye99 <kwakyedarko@gmail.com>
2022-03-12 11:13:58 -0800

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main -Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char r;
+for (r = 'a'; r <= 'z'; r++)
+{
+if (r != 'e' && r != 'q')
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
+}
