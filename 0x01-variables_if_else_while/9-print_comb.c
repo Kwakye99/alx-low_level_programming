b3164b9f903cd588d7fec8c0a6ac5caafc3d86cd
Kwakye99 <kwakyedarko@gmail.com>
2022-03-12 15:25:13 -0800

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Return: ALways 0 (Success)
+*/
+int main
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+if (c != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
