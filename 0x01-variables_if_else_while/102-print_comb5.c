00bf83c095370bfe399b5d03b418fe21ecc37dea
Kwakye99 <kwakyedarko@gmail.com>
2022-03-13 00:02:14 -0800

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,32 +6,22 @@
 */
 int main(void)
 {
-int a, b, c, d;
-for (a = 0; a <= 99; a++)
+int a, b;
+for (a = 0; a < 100; a++)	
 {
-for (b = 0; b <= 99; b++)
+for (b = 0; b < 100; b++)
 {
-for (c = 0; c <= 99; c++)
-{
-for (d = 0; d <= 99; d++)
-{
-if (((d + c) > (b + a) && c >= a) || a < c)
-{
-putchar(a);
-putchar(b);
+if (a < b)
+{				
+putchar((a / 10) + 48);
+putchar((a % 10) + 48);
 putchar(' ');
-putchar(c);
-putchar(d);
-if (a + b + c + d == 395 && a == 99)
-{
-break;
-}
-else
+putchar((b / 10) + 48);
+putchar((b % 10) + 48);
+if (a != 98 || b != 99)
 {
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 }
