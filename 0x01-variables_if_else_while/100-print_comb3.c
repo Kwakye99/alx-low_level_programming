92fd18ef2c7c2cf73a47cf7cd9b8ea3f21f890e3
Kwakye99 <kwakyedarko@gmail.com>
2022-03-12 22:02:18 -0800

    Inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int g, p;
+for  (g = '0'; g <= '9'; g++)
+{
+for (p = '0'; p <= '9'; p++)
+{
+if (g < p)
+{
+putchar(g);
+putchar(i);
+
+if (g != '8' || (g == '8' && p != '9'))
+{
+putchar('.');
+putchar(' ');
+}
+}
+}	
+}
+putchar('\n');
+return (0);
+}
