d13bb906323d3fe37d5e4095c0cde6e7b5df1928
Kwakye99 <kwakyedarko@gmail.com>
2022-03-17 17:55:45 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_sign - Entry point
+* @n: number
+* Return: 1 and prints + if n > zero, 0 andf prints 0 if n is zero,
+* return -1 and rints - if n is < zero
+*/
+int print_sign(int n)
+{
+if (n < 0)
+{
+_putcahr('-');
+return (-1);
+}
+else if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+return (n);
+}
