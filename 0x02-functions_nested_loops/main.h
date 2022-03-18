b5211c3e64c1be8d014a2c237739d22c603a864b
Kwakye99 <kwakyedarko@gmail.com>
2022-03-17 19:12:09 -0700

    Changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,8 +11,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
-int print_to_98(int n);
+int add(int a, int b);
+void print_to_98(int n);
 void print_times_taable(int n);
 
 #endif
