eccfb14bf843a69cff3f5856c2f32be37141f565
Kwakye99 <kwakyedarko@gmail.com>
2022-03-17 17:26:35 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isalpha -Entry point
+* @c: Character in ASCII
+* Return: Always 1 when c is uppercase, else 0
+*/
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else 
+return (0);
+}
