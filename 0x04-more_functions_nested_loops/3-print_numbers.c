dcf3f1662597299acf49c9cc92a1b81bafcbb49a
Kwakye99 <kwakyedarko@gmail.com>
2022-03-20 01:00:28 -0700

    Prints numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * print_numbers - my function name
 * void: inputs not specified
@@ -8,7 +7,7 @@
 void print_numbers(void)
 {
 int me;
-for (me = 0; me <= 9; me++)
+for (me = 0; me < 10; me++)
 {
 _putchar(me);
 }
