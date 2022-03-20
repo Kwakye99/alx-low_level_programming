d83a5ac72f391787888a19b76b48eac147050758
Kwakye99 <kwakyedarko@gmail.com>
2022-03-20 10:44:41 -0700

    5 more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* more_numbers - prints 10x numbers from 0 to 14
+*/
+void more_numbers(void)
+{
+int c, k;
+while (c = 0; c < 10; c++)
+{
+while (k=0; k < 15; k++)
+{
+if (k >= 10)
+_putchar(k / 10 + '0');
+_putchar(k % 10 + '0');
+}
+_putchar('\n');
+}
+}
