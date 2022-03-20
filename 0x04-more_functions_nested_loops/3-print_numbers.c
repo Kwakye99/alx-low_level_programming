6e1969c92d5c3bbe0095719196558064fdade5e8
Kwakye99 <kwakyedarko@gmail.com>
2022-03-20 00:49:19 -0700

    Prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_numbers - my function name
+* void: inputs not specified
+* Return: void
+*/
+void print_numbers(void)
+{
+int me = 0;
+for (me <= 9; me++)
+{
+_putchar(me);
+}
+putchar('\n');
+}
