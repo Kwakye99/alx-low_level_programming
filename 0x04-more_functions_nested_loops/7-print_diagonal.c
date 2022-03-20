3341ea4061a1b7728ccb77e89b9ae2405ac3225d
Kwakye99 <kwakyedarko@gmail.com>
2022-03-20 11:42:36 -0700

    diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_diagonal - use to print diagonal
+* Return: void 
+*/
+void print_diagonal(int n)
+{
+if (n <= 0)
+{
+_printchar('\n');
+}
+else
+int k, m;
+for (k = 0; k < n; k++)
+{
+for (m = 0; m < n; m++)
+{
+if (m == k)
+_putchar('\\');
+else if (m < k)
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
+}
