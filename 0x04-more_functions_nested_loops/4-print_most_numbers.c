40c49464999eab74dfd1fdb314d4484cc9a422d6
Kwakye99 <kwakyedarko@gmail.com>
2022-03-20 10:11:24 -0700

    I believe

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_most_numbers - printing numbers from 0 to 9
+* Return: Alwyas void
+*/
+void print_most_numbers(void)
+{
+int c;
+for (c = o; c < 10; c++, c != 2, 4)
+{
+_putchar(c + '0');
+}
+_putchar('\n');
+}
